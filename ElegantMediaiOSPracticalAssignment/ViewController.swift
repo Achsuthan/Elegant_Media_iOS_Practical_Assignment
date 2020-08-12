9510049d9e28f16cc6768d77e95094bd150d6da8
Achsuthan <achsuthan@icloud.com>
2020-08-12 20:01:50 +0530

    Initial Commit

---
+++ ElegantMediaiOSPracticalAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElegantMediaiOSPracticalAssignment
+//
+//  Created by Achsuthan Mahendran on 8/12/20.
+//  Copyright © 2020 Achsuthan Mahendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
